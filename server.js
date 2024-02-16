bbb4968c4f9bc102f207b2580e56572e1e302cfc
archakorn <achakorn9909@gmail.com>
2024-02-16 10:38:29 +0700

    book-id

--- server.js
+++ server.js
@@ -2,14 +2,18 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hello World')
+    res.send('Hello World')
 })
 
 const books = require('./db')
 app.get('/books', (req, res) => {
-  res.json(books)
+    res.json(books)
+})
+
+app.get('/books/:id', (req, res) => {
+    res.json(books.find(book => book.id === req.params.id))
 })
 
 app.listen(3000, () => {
-  console.log('http://localhost:3000')
+    console.log('http://localhost:3000')
 })
