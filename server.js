9abe21642d9c9956cd1c763f344492c0385ede30
archakorn <achakorn9909@gmail.com>
2024-03-10 21:19:09 +0700

    Initial commit

--- server.js
+++ server.js
@@ -6,30 +6,39 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 
 app.get('/', (req, res) => {
-    res.send('Hello World')
+    res.status(200).send('Archakorn Kajeechrekun 6304101380')
 })
 
 const books = require('./db')
 app.get('/books', (req, res) => {
-    res.json(books)
+    res.status(200).json(books)
 })
 
+//select * from books
 app.get('/books/:id', (req, res) => {
-    res.json(books.find(book => book.id === req.params.id))
+    res.status(200).json(books.find(book => book.id === req.params.id))
 })
 
+//insert into books ()
 app.post('/books', (req, res) => {
     books.push(req.body)
+    res.status(201).json(req.body)
 })
 
+//update 
 app.put('/books/:id', (req, res) => {
+    //res.send("Hello Kitty")
     const updateIndex = books.findIndex(book => book.id === req.params.id)
-    res.json(Object.assign(books[updateIndex], req.body))
-})
-
+    res.status(200).json(Object.assign(books[updateIndex], req.body))
+    
+  })
+  
+  
+//delete from * where 
 app.delete('/books/:id', (req, res) => {
     const deletedIndex = books.findIndex(book => book.id === req.params.id)
-    delete books[deleteIndex];
+    delete books[deletedIndex];
+    res.json(req.body)
 })
 
 app.listen(3000, () => {
